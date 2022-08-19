79fe76d7b5473e2b6c57fd31244982f081483b4d
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-19 10:53:59 +0800

    feat(controller): :sparkles: add index in StudentController

--- backend/app/Http/Controllers/StudentController.php
+++ backend/app/Http/Controllers/StudentController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    public function index()
+    {
+        $data = Student::all();
+        return response()->json([
+            "success" => true,
+            "message" => "Successfully retrieved all students",
+            "data" => $data
+        ]);
+    }
 }
