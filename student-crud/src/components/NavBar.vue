f326c89b957b51dc2c96cf9e5050d67aa15e3cf1
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-19 21:36:48 +0800

    feat(components): :lipstick: add navbar

---
+++ student-crud/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <h1>Student CRUD</h1>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background-color: blueviolet;
+  color: white;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+</style>
