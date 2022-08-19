780089891fc9f7d67dfec47a3141b62ea4cdd4cd
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-19 21:40:35 +0800

    Add Addview

--- student-crud/src/router/index.js
+++ student-crud/src/router/index.js
@@ -9,12 +9,10 @@ const router = createRouter({
       component: () => import("../views/StudentsView.vue"),
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("../views/Addview.vue"),
     },
   ],
 });
